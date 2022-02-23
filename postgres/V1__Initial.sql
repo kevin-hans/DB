a12193fc6400805d7e33d54166610e230bf98a92
kevin <hncen@163.com>
2022-02-24 00:05:15 +0900

    postgresql

---
+++ postgres/V1__Initial.sql
@@ -0,0 +1,13 @@
+CREATE SCHEMA IF NOT EXISTS xshema;
+CREATE TABLE IF NOT EXISTS employee (
+    empId CHAR(8),
+    empName CHAR(100),
+    PRIMARY KEY(empId)
+);
+
+
+
+
+
+
+
---
+++ postgres/V2__data.sql
@@ -0,0 +1,5 @@
+TRUNCATE TABLE xshema.employee;
+INSERT INTO xshema.employee (empId, empName) VALUES ('1', 'scott');
+INSERT INTO xshema.employee (empId, empName) VALUES ('2', 'jack');
+INSERT INTO xshema.employee (empId, empName) VALUES ('3', 'tom');
+INSERT INTO xshema.employee (empId, empName) VALUES ('4', 'bill');
